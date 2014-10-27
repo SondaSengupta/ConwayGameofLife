33a879be5e588d9d147484dfc9d624d065e8bc5e
SondaSengupta <sondasengupta@gmail.com>
2014-10-27 16:55:34 -0500

    created table using for.Each loops

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-document.addEventListener('DOMContentLoaded', function(){
-
 var matrix = [ [0,1,0], [1,1,0], [1,1,1] ];
 
-for (var i = 0; i < 3; i++){
-  addToTable("grid");
-}
-
-function addToTable(tableID){
-  var table = document.getElementById(tableID);
-  var newRow = table.insertRow(0);
-  var cell1 = newRow.insertCell(0);
-  var newText1 = document.createTextNode(matrix[i]);
-  cell1.appendChild(newText1);
-}
-
+document.addEventListener('DOMContentLoaded', function(){
+  table.innerHTML = '';
+  matrix.forEach(creatingTable);
 });
+
+function creatingTable(element){
+  var table = document.querySelector('#table');
+  $tr = document.createElement('tr');
+  table.appendChild($tr);
+  element.forEach(function() {
+      var $td = document.createElement('td');
+      $tr.appendChild($td);
+      });
+}
