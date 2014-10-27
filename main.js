3c9c5814a28dcb1a2f972d6fe279dc3c739825ed
SondaSengupta <sondasengupta@gmail.com>
2014-10-27 17:39:45 -0500

    Represented cells has green and black boxes

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 var matrix = [ [0,1,0], [1,1,0], [1,1,1] ];
+var $td
+
 
 document.addEventListener('DOMContentLoaded', function(){
   table.innerHTML = '';
   matrix.forEach(creatingTable);
 });
 
-function creatingTable(element){
+function creatingTable(row){
   var table = document.querySelector('#table');
   $tr = document.createElement('tr');
   table.appendChild($tr);
-  element.forEach(function() {
-      var $td = document.createElement('td');
+  row.forEach(function(cell) {
+      $td = document.createElement('td');
       $tr.appendChild($td);
-      });
+      $td.style.width = "40px";
+      $td.style.height = "40px";
+        if (cell === 1){
+          $td.style.backgroundColor="black";
+        }
+        else {
+          $td.style.backgroundColor="green";
+        }
+    });
+}
+
+function matrixRandom(x,y){
+  var matrix=[];
+  for (var i=0; i < x; i++) {
+    matrix[i]=[];
+    for(var j=0; j < y; j++) {
+      matrix[i][j]=Math.round(Math.random());
+    }
+  }
+  return matrix
 }
